1755d220848796f17a846f60798c3793d47b2e83
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-06-21 19:06:07 -0300

    created command --lofime me inside of the indexjs file

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { getVoiceConnection } from '@discordjs/voice';
+import Bot from './Bot.js';
+import Commands from './components/Commands/Commands.js'
+
+
+Bot.client.on('ready', () => {
+  console.log('Bot is ready sir.')
+})
+
+Bot.client.on('messageCreate', async (message) => {
+  if (!message.author.bot) {
+    if (message.content == '--lofime') {
+      const channel = message.member.voice.channel;
+      if (channel) {
+        try {
+          await Commands.lofime(channel)
+          message.reply('Playing...');
+        } catch (error) {
+          throw error;
+        }
+      } else {
+        message.reply('Join a voice channel and try again.');
+      }
+    } else if (message.content == '--leave') {
+      const channel = message.guild.id;
+      try {
+        const connection = getVoiceConnection(channel);
+        connection.destroy();
+      } catch(error) {
+        message.reply('No player found')
+      }
+    }
+  }
+});
+
+
+Bot.client.login(process.env.DS_TOKEN);
