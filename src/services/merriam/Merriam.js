af15bbdfae48613e8f362a62559889e39967d003
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-06-23 18:53:35 -0300

    added Merriam class and method to get the definition of a word

---
+++ src/services/merriam/Merriam.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import qs from 'querystring'
+import dotenv from 'dotenv'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path'
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path:  path.join(__dirname, '..', '..', '..', '.env')})
+
+const baseUrl = 'https://www.dictionaryapi.com/api/v3/references/learners/json'
+
+class Merriam {
+  async getWord(word) {
+    const fullUrl = `${baseUrl}/${word}?key=${process.env.MERRIAM_LEARNER_API_KEY}`
+    const response = await axios.get(fullUrl)
+    console.log(response.data)
+    return response.data
+  }
+}
+
+export default new Merriam()
